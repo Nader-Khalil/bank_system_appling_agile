8391ef35ed92b6da07ea0650bd94dd29839d45da
Yassmina-Abdo <yassmenaabdo6@gmail.com>
2022-05-09 15:28:44 +0200

    add fns to admin file

---
+++ admin.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct client
+{
+    char* name;
+    int id;
+    int account_no;
+    int password;
+
+    
+}Client;
+
+void admin_init(Client* person);
+void admin_Login();
+void admin_CreateAccount();
+void admin_Logout();
+void admin_EditProfile();
+void admin_MakeTranaction();
+void admin_ChangeAccountStatus();
+void admin_GetCash();
+void admin_DepositInAccount();
+void admin_ReturnToMainMenu();
+
+
+
+void admin_init(Client* person)
+{
+person->name =" ";
+person->id =NULL;
+person->account_no =NULL;
+person->password =NULL;
+}
+
+void admin_Login(){}
+void admin_CreateAccount(){}
+void admin_Logout(){}
+void admin_EditProfile(){}
+void admin_MakeTranaction(){}
+void admin_ChangeAccountStatus(){}
+void admin_GetCash(){}
+void admin_DepositInAccount(){}
+void admin_ReturnToMainMenu(){}
