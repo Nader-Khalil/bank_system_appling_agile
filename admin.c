e8361f2ce353413b95174183b6105473a2392248
Yassmina-Abdo <yassmenaabdo6@gmail.com>
2022-05-09 16:58:35 +0200

    add admin login function.

--- admin.c
+++ admin.c
@@ -21,7 +21,7 @@ typedef struct admin
 } Admin;
 
 void admin_init(Admin *person);
-void admin_Login();
+void admin_Login(Admin* person);
 void admin_CreateAccount();
 void admin_Logout();
 void admin_EditProfile();
@@ -33,13 +33,25 @@ void admin_ReturnToMainMenu();
 
 void admin_init(Admin *person)
 {
-    person->name = " ";
-    person->id = NULL;
-    person->account_no = NULL;
-    person->password = NULL;
+    person->name = "nader";
+    person->id = 1;
+    person->account_no = 12133254;
+    person->password = 12345678;
 }
 
-void admin_Login() {}
+void admin_Login(Admin* person)
+{   char* username;
+    int password;
+   
+    printf("Please Enter Admin UserName : ");
+    scanf("%c", &username);
+
+    printf("Please Enter Admin Password : ");
+    scanf("%d", &password);
+
+    if (username == person->name && password == person->password){ printf("WElcome to Admin Window \n");}
+    else{printf(" Invalid username or password");}
+}
 void admin_CreateAccount() {}
 void admin_Logout() {}
 void admin_EditProfile() {}
