3f0d3db25db3f2351f004d053ea94ec795d02052
Yassmina-Abdo <yassmenaabdo6@gmail.com>
2022-05-10 21:26:35 +0200

    update in main class.

--- main.c
+++ main.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
+#include "admin.c"
+#include <stdlib.h>
+#include <string.h>
+#include "account.c"
   
 // main function -
 // where the execution of program begins
 int main()
 {
+  //Admin admin;
+   printf("pass %d \n",30);
+   
+  Account account = account_init() ;
   
-    // prints hello world
-    printf("Hello World");
+     printf("age %d \n",account.age);
+      //printf("id %c \n",account.accountID);
+
+ 
+   
+  //account_init(acc);
+  int a=30;
+  printf("pass %d \n",30);
+  printf("hallo"); 
   
     return 0;
 }
