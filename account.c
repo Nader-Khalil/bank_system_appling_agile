0ea17465dd718962b71b5adb8147d852c310a73f
Yassmina-Abdo <yassmenaabdo6@gmail.com>
2022-05-10 23:30:08 +0200

    update in account file.

--- account.c
+++ account.c
@@ -3,19 +3,28 @@
 #include <string.h>
 #include <time.h>
 
-typedef struct 
+typedef struct account
 {
-    char *fullName;
+    char* fullName;
     char *fullAddress;
     int nationalID;
     int age;
     char *accountID;
-    int guardian;
+    char* guardian;
     int guardianNationalID;
     char *status;
     char *balance;
     char *password;
+    struct account* Next;
 } Account;
+
+
+typedef struct 
+{
+   Account* head;
+   int size;
+} AccountLinkedList;
+
 
 
 Account account_init();
@@ -27,6 +36,7 @@ char* generate_Password(int password_length);
 Account account_init()
 {
   Account newAccount;
+  newAccount.fullName =" ";
   newAccount.password = generate_Password(8);
   newAccount.accountID = generate_AccountID(10); 
   return newAccount;
@@ -36,10 +46,10 @@ char* generate_Password(int password_length)
 {
     char list[] = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$^&*()_- +=QWERTYUIOPASDFGHJKLZXCVBNM[]{};':\"<>,.?/";
     srand(time(NULL));
-    char* str;
+    char* str = (char*) malloc(sizeof(char) * password_length);
     for(int i = 0; i < password_length; i++) 
     {
-        //str +i = list[rand() % (sizeof list - 1)];
+        str[i] = list[rand() % (sizeof list - 1)];
         
     }
     return str;
@@ -49,7 +59,7 @@ char* generate_AccountID(int id_length)
 {
     char list[] = "1234567890";
     srand(time(NULL));
-    char* str;
+    char* str = (char*) malloc(sizeof(char) * id_length);;
     for(int i = 0; i < id_length; i++) 
     {
         str[i] = list[rand() % (sizeof list - 1)];
